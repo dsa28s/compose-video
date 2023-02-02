de27b617dc140224bb95afcf97c170b7e05bb8f5
Dora Lee <dsa28s@naver.com>
2023-02-02 19:20:11 +0900

    style: apply ktlint

--- compose-video-baselineprof/src/main/kotlin/io/sanghun/compose/video/benchmark/BaselineProfileBenchmark.kt
+++ compose-video-baselineprof/src/main/kotlin/io/sanghun/compose/video/benchmark/BaselineProfileBenchmark.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 Dora Lee
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.sanghun.compose.video.benchmark
 
 import androidx.benchmark.macro.BaselineProfileMode
@@ -9,7 +24,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
 
 @RunWith(AndroidJUnit4::class)
 class BaselineProfileBenchmark {
@@ -26,8 +40,8 @@ class BaselineProfileBenchmark {
     fun startupBaselineProfile() {
         startup(
             CompilationMode.Partial(
-                baselineProfileMode = BaselineProfileMode.Require
-            )
+                baselineProfileMode = BaselineProfileMode.Require,
+            ),
         )
     }
 
@@ -37,7 +51,7 @@ class BaselineProfileBenchmark {
             metrics = listOf(StartupTimingMetric()),
             iterations = 10,
             startupMode = StartupMode.COLD,
-            compilationMode = compilationMode
+            compilationMode = compilationMode,
         ) {
             pressHome()
             startActivityAndWait()
