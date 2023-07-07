aa98a058ee696cb7f428696c0205893f5694332e
Dora Lee <dsa28s@naver.com>
2023-07-07 18:36:05 +0900

    dependencies: update android-build-tools 7.3.0 -> 7.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
 
 plugins {
-    id("com.android.application") version "7.3.0" apply false
-    id("com.android.library") version "7.3.0" apply false
+    id("com.android.application") version "7.4.2" apply false
+    id("com.android.library") version "7.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 
     id("com.diffplug.spotless") version "6.14.0"
